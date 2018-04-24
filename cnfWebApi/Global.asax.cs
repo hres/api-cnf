bf56a9f07b48a5091b3e94c615dd1fafc4e215c2
Barrance <barry.martin@canada.ca>
2018-04-24 08:21:18 -0400

    Added missing hidden ddl files.

--- cnfWebApi/Global.asax.cs
+++ cnfWebApi/Global.asax.cs
@@ -1,14 +1,7 @@
-﻿//using cnfWebApi;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
-using System.Web;
+﻿
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
-using System.Web.Routing;
 
 namespace cnfWebApi
 {
