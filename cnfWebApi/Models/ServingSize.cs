9f6225cabb13576ddd4f898900e0d932f3deb8a5
Barrance <barry.martin@canada.ca>
2018-05-03 14:12:21 -0400

    Updated key names for datasets, changed SQL commandtext for security, removed CSV format, removed index test page, formatted and commented code.

--- cnfWebApi/Models/ServingSize.cs
+++ cnfWebApi/Models/ServingSize.cs
@@ -3,10 +3,11 @@ namespace cnfWebApi.Models
 {
     public class ServingSize
     {
-        public double conv_factor { get; set; }
-        public int food_c { get; set; }
-        public string food_desc { get; set; }
-        public string measure_desc { get; set; }
-        public string measure_type { get; set; }
+        public double conversion_factor_value { get; set; }
+        public int food_code { get; set; }
+        public string food_description { get; set; }
+        public string measure_name { get; set; }
+        // 20180501 measure_type is not being displayed to reduce data set size.
+        //public string measure_type { get; set; }
     }
 }
