e9b8e8a095b18f3deb6d49514a0487d821edf4f0
alex gagnon <alexgagnon@users.noreply.github.com>
2019-06-03 13:55:05 -0400

    fix global exception handler (#9)
    * Change to relative dir for logs
    
    * global exception handler and fix null pointer exceptions
    
    * remove logs
    
    * remove logs

--- cnfWebApi/App_Start/WebApiConfig.cs
+++ cnfWebApi/App_Start/WebApiConfig.cs
@@ -34,6 +34,8 @@ namespace cnfWebApi
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
             config.EnableSystemDiagnosticsTracing();
+
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.LocalOnly;
         }
     }
 }
